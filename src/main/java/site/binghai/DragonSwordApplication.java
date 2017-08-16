ed404c3d9fb71f2b4f5a8f9a8f604cb78d60a5f2
icesea <binghai@meili-inc.com>
2017-08-16 10:58:42 +0800

    Initial commit

---
+++ src/main/java/site/binghai/DragonSwordApplication.java
@@ -0,0 +1,12 @@
+package site.binghai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DragonSwordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DragonSwordApplication.class, args);
+	}
+}
